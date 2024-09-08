16a0f9df329b46309c3f4c54f1c5580fad02083e
crin03 <jyt2097@gmail.com>
2024-09-08 22:40:11 +0900

    회원가입 / 로그인, 로그아웃 - INIT

---
+++ src/main/java/me/rightnow/springbootdeveloper/controller/TokenApiController.java
@@ -0,0 +1,25 @@
+package me.rightnow.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.rightnow.springbootdeveloper.dto.CreateAccessTokenRequest;
+import me.rightnow.springbootdeveloper.dto.CreateAccessTokenResponse;
+import me.rightnow.springbootdeveloper.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
---
+++ src/main/java/me/rightnow/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.rightnow.springbootdeveloper.repository;
+
+import me.rightnow.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
