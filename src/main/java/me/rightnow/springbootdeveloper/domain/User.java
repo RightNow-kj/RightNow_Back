5b779a76461e40ad5e2cbb2f6d10fc0f02b62e34
crin03 <jyt2097@gmail.com>
2024-09-11 15:18:42 +0900

    OAuth 클라이언트 ID 비번 추가

--- src/main/java/me/rightnow/springbootdeveloper/domain/User.java
+++ src/main/java/me/rightnow/springbootdeveloper/domain/User.java
@@ -12,10 +12,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+// users 테이블과 연결되는 엔티티 클래스임을 나타냄
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-@Entity
+@Getter  // 클래스의 모든 필드에 대해 getter 메서드를 자동으로 생성
+@Entity  // 클래스가 JPA 엔티티임을 나타냄
+// UserDetail를 상속받아 인증 객체로 사용
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,37 +36,44 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-    @Override
+    @Override // 권한 반환
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("user"));
     }
 
+    // 사용자의 id를 반환(고유한 값)
     @Override
     public String getUsername() {
         return email;
     }
 
+    // 사용자의 패스워드 반환
     @Override
     public String getPassword() {
         return password;
     }
+
+    // 계정 만료 여부 반환
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return true; // true -> 만료되지 않음
     }
 
+    // 계정 잠금 여부 반환
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return true; // true -> 잠금되지 않음
     }
 
+    // 패스워드의 만료 여부 반환
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return true; // true -> 만료되지 않음
     }
 
+    // 계정 사용 가능 여부 반환
     @Override
     public boolean isEnabled() {
-        return true;
+        return true; // true -> 사용 가능
     }
 }
