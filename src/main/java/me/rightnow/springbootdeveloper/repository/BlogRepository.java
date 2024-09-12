a471bc953f0b6b78ed50056a74a0dd5bd3368a98
wonabee <jiwon8260@gmail.com>
2024-09-12 23:28:09 +0900

    스프링 시큐리티로 OAuth2 구현_수정본

--- src/main/java/me/rightnow/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.rightnow.springbootdeveloper.repository;
-
-import me.rightnow.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
